cbb7f067d828669f6078491a51cd31c5ac1820ef
YisehakE <yisehakebrahim02@gmail.com>
2023-11-26 10:35:40 -0500

    Update for comparasion

--- lab-link-layer/switch.py
+++ lab-link-layer/switch.py
@@ -26,15 +26,13 @@ class Switch(BaseHost):
         for myint in self.physical_interfaces:
           val = vlan_info[myint]
           if val == 'trunk':
-            #TODO: Add one line of code to handle this if statement.
-            self._trunks.add(myint) # ------- TODO: check if logic correct
+            self._trunks.add(myint)
           else:
-            vlan_val = int(val[4:]) # Extract the VLAN id from 802.1Q header ------- TODO: check if logic correct
+            vlan_val = int(val[4:]) # Extract the VLAN id from 802.1Q header
             if vlan_val not in self._vlans:
-                #TODO: Add one line of code to handle this if statement.
-                self._vlans[vlan_val] = [] # Initialize to empty list so interfaces can be ------- TODO: check if logic correct
+                self._vlans[vlan_val] = [] # Initialize to empty list so interfaces on same VLAN grouping can be added later on 
 
-            self._vlans[vlan_val].append(myint) # ------- TODO: check if logic correct
+            self._vlans[vlan_val].append(myint)
             self._intfs[myint] = vlan_val
       else:
         for myint in self.physical_interfaces:
@@ -42,64 +40,46 @@ class Switch(BaseHost):
           if vlan_val not in self._vlans:
               self._vlans[vlan_val] = []
           
-          #TODO: Finish filling in the code for the for loop. Hint: it has to do with handling vlan_val and is only 2 lines of code.
           self._vlans[vlan_val].append(myint)
           self._intfs[myint] = vlan_val
 
 
     
     def create_802_1Q_frame(self, vlan_id):
-       
        frame = b''
 
        frame += struct.pack("!H", VLAN_1Q_IDENTIFIER)
        all_ones_16_bit = 2**16 - 1
-       frame += struct.pack("!H", all_ones_16_bit & vlan_id) # Idea is 0000-0000-0000 | ####-####-#### => 0000 - ####-####-#### where vlan_id is represents the the 12 hashtags!
+       frame += struct.pack("!H", all_ones_16_bit & vlan_id) # Idea is 1111-1111-1111 & ####-####-#### => 0000 - ####-####-#### where vlan_id is represents the the 12 hashtags!
 
        return frame
-    
-    def create_ethernet_frame(self, dst, src, eth_type, payload, vlan_802_1Q_hdr = None):
-       
-      frame = b''
-      frame += dst if type(dst) is bytes else struct.pack("!13s", dst)
-      frame += src if type(dst) is bytes else struct.pack("!13s", src)
 
-      frame += vlan_802_1Q_hdr if vlan_802_1Q_hdr else b''
-
-      frame += eth_type if type(eth_type) is bytes else struct.pack("!H", eth_type)
-      frame += payload if type(payload) is bytes else bytes(payload)
-
-      return frame
 
     def _handle_frame(self, frame: bytes, intf: str) -> None:
       src = frame[6:12]
       dst = frame[:6]
 
-      type = frame[12:14] or frame[18:20] # The latter is if frame includes 802.1Q header 
-      payload = frame[14:] or frame[20:] # The latter is if frame includes 802.1Q header 
-      vlan = None # Initialize vlan to be used in other parts of handle frame 
+      vlan = None # Initialize VLAN to be used in other parts of handle frame 
 
       if intf in self._trunks:
-        vlan = struct.unpack('!H', frame[14:16])[0]
+        vlan = struct.unpack('!H', frame[14:16])[0] # Get VLAN of intf that this frame was received on from 802 header 
         frame = frame[:12] + frame[16:]
       else:
-        vlan = self._intfs[intf] # Why do we set interface mapping to a VLAN even if this switch is not a trunk
+        vlan = self._intfs[intf] # Get the VLAN of intf that this frame was received on from mapping if not a trunk port
 
       if dst in self._outgoing:
         if self._outgoing[dst] in self._trunks:
-          #TODO: complete the following line
-          frame = dst + src + self.create_802_1Q_frame(vlan) + frame[16:]
+          frame = dst + src + self.create_802_1Q_frame(vlan) + frame[16:] # Need to send header if sending to outgoing trunk port
 
         self.send_frame(frame, self._outgoing[dst])
       else:
         for myint in self.physical_interfaces:
           if intf != myint and (myint in self._trunks or self._intfs[myint] == vlan):
             if myint in self._trunks:
-              #TODO: Complete the following line
-              fr = dst + src + self.create_802_1Q_frame(vlan) + frame[16:]
+              fr = dst + src + self.create_802_1Q_frame(vlan) + frame[16:] # Need to send header if sending to other interface(s) that are trunk ports
 
             else:
-              fr = frame # Already stripped from frame when checking on receiving end that this port isn't trunk ---- TODO: check if logic is correct
+              fr = frame # Already stripped from frame when checking on receiving end that this port isn't trunk
 
             self.send_frame(fr, myint)
         
@@ -110,11 +90,10 @@ class Switch(BaseHost):
       if ev is not None:
           ev.cancel()
       loop = asyncio.get_event_loop()
-      #Complete the following line/
+
       self._remove_events[src] = loop.call_later(AGING_TIME, self.del_outgoing, src)
 
     def del_outgoing(self, src):
-        #TODO: Complete the following line.
         del self._outgoing[src]
 
 def main():
